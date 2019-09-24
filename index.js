920abd671b25531f5e2e652e1610c312ce6151a8
Jonathan.e <jonathanerquinigo@pursuit.org>
2019-09-24 12:31:14 -0400

    adding multiple songs as a playlist

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
 document.addEventListener("DOMContentLoaded", ()=> {
-
+    let audioPlayer = document.getElementById("audioPlayer")
+    let playlist = document.getElementById("playlist")
+    let item1 = document.getElementById("item1")
+    let item2 = document.getElementById("item2")
+    let song1 = document.getElementById("song1")
+    let button = document.getElementById("nextButton")
     
+    item1.addEventListener("click", e => {
+        audioPlayer.src = e.target.getAttribute("href")
+
+    })
+
 })
